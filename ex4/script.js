bde0f233829c9157cbcc7274db670290c2f0d89e
Sue Park <suepandrice06@gmail.com>
2019-08-02 21:41:28 -0500

    completed exercise 4

--- ex4/script.js
+++ ex4/script.js
@@ -1,2 +1,22 @@
 // Make all of the boxes blue when you click the button
 // Notice that all the boxes have "class" instead of "id"
+let button = document.getElementById("myButton")
+
+button.addEventListener('click', function(){
+
+    let divs = document.querySelectorAll(".box")
+
+    for(let i = 0; i < divs.length; i++) {
+        divs[i].style.background = "blue";
+    }
+
+    // divs.forEach(function(box){
+    //     box.style.background = "blue"
+    // })
+
+    
+});
+
+
+
+// style.background = "blue"
